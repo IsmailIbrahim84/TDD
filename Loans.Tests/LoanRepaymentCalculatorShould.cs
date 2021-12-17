215d687c0d4f6675ba95d3e0dfd9b54b130b52d3
IsmailIbrahim84 <beisso75@hotmail.com>
2021-12-17 21:53:08 +0200

    latest

--- Loans.Tests/LoanRepaymentCalculatorShould.cs
+++ Loans.Tests/LoanRepaymentCalculatorShould.cs
@@ -8,19 +8,22 @@ namespace Loans.Tests
 {
     public class LoanRepaymentCalculatorShould
     {
-        private LoanRepaymentCalculator _sut;
-        private decimal _monthlyPayment;
+        //private LoanRepaymentCalculator _sut;
+        //private decimal _monthlyPayment;
 
         [SetUp]
         public void Setup()
         {
-             _sut = new LoanRepaymentCalculator();
-             _monthlyPayment = _sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
+             //_sut = new LoanRepaymentCalculator();
+           //  _monthlyPayment = _sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
         }
 
         [Test]
         public void CalculateCorrectMonthlyRepayment()
         {
+            var _sut = new LoanRepaymentCalculator();
+            var _monthlyPayment = _sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
+        
             Assert.That(_monthlyPayment,Is.EqualTo(1264.14));
         }
     }
