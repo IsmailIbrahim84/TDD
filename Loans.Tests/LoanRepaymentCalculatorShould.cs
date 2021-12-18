bfcfc670e407428a029cd6f30ecdf9eef90eee90
IsmailIbrahim84 <beisso75@hotmail.com>
2021-12-18 17:02:55 +0200

    Add test cases

--- Loans.Tests/LoanRepaymentCalculatorShould.cs
+++ Loans.Tests/LoanRepaymentCalculatorShould.cs
@@ -26,5 +26,26 @@ namespace Loans.Tests
         
             Assert.That(_monthlyPayment,Is.EqualTo(1264.14));
         }
+
+        [Test]
+        [TestCase(200_000, 6.5, 30, 1264.14)]
+        public void CalculateCorrectMonthlyRepayment_Parameters(decimal principal, decimal interestRate, int termInYears, decimal expectedMonthlyPayment)
+        {
+            var _sut = new LoanRepaymentCalculator();
+            var _monthlyPayment = _sut.CalculateMonthlyRepayment(new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+        
+            Assert.That(_monthlyPayment,Is.EqualTo(expectedMonthlyPayment));
+        }
+
+        [Test]
+        [TestCase(200_000, 6.5, 30,ExpectedResult= 1264.14)]
+        public decimal CalculateCorrectMonthlyRepayment_Expected(decimal principal, decimal interestRate,
+            int termInYears)
+        {
+            var _sut = new LoanRepaymentCalculator();
+            return _sut.CalculateMonthlyRepayment(new LoanAmount("USD", principal), interestRate,
+                new LoanTerm(termInYears));
+
+        }
     }
 }
