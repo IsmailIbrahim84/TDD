ecb4d87cda81fe5078eb9621a81ce79906e3d6f2
IsmailIbrahim84 <beisso75@hotmail.com>
2022-01-21 23:07:40 +0200

    Adding Moc

---
+++ Loans.Tests/LoanApplicationProcessorShould.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Loans.Domain.Applications;
+using Moq;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    public class LoanApplicationProcessorShould
+    {
+        [Test]
+        public void DeclineLowSalary()
+        {
+            LoanProduct product = new LoanProduct(99, "Loan", 5.25m);
+            LoanAmount amount = new LoanAmount("USD", 200_000);
+            var application = new LoanApplication(42,
+                                                  product,
+                                                  amount,
+                                                  "Sarah",
+                                                  25,
+                                                  "133 Pluralsight Drive, Draper, Utah",
+                                                  64_999);
+            var mocIdentityVerifier = new Mock<IIdentityVerifier>();
+            var mocCreditScore = new Mock<ICreditScorer>();
+
+            var sut = new LoanApplicationProcessor(mocIdentityVerifier.Object, mocCreditScore.Object);
+
+            sut.Process(application);
+
+            Assert.That(application.GetIsAccepted(), Is.False);
+        }
+
+        [Test]
+        public void Accept()
+        {
+            LoanProduct product = new LoanProduct(99, "Loan", 5.25m);
+            LoanAmount amount = new LoanAmount("USD", 200_000);
+
+            
+            var application = new LoanApplication(42,
+                product,
+                amount,
+                "Sarah",
+                25,
+                "133 Pluralsight Drive, Draper, Utah",
+                65_000);
+
+            var mocIdentityVerifier = new Mock<IIdentityVerifier>();
+
+            mocIdentityVerifier.Setup(x => x.Validate("Sarah", 25,  "133 Pluralsight Drive, Draper, Utah")).Returns(true);
+
+            var mocCreditScore = new Mock<ICreditScorer>();
+            
+            var sut = new LoanApplicationProcessor(mocIdentityVerifier.Object, mocCreditScore.Object);
+
+            sut.Process(application);
+
+            Assert.That(application.GetIsAccepted(), Is.True);
+
+        }
+
+    }
+
+    }
--- Loans.Tests/LoanRepaymentCalculatorShould.cs
+++ Loans.Tests/LoanRepaymentCalculatorShould.cs
@@ -1,62 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Loans.Domain.Applications;
+﻿using Loans.Domain.Applications;
 using NUnit.Framework;
+using System;
 
 namespace Loans.Tests
 {
     public class LoanRepaymentCalculatorShould
     {
-        //private LoanRepaymentCalculator _sut;
-        //private decimal _monthlyPayment;
-
-        [SetUp]
-        public void Setup()
-        {
-             //_sut = new LoanRepaymentCalculator();
-           //  _monthlyPayment = _sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
-        }
-
-        [Test]
-        public void CalculateCorrectMonthlyRepayment()
-        {
-            var _sut = new LoanRepaymentCalculator();
-            var _monthlyPayment = _sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
-        
-            Assert.That(_monthlyPayment,Is.EqualTo(1264.14));
-        }
-
         [Test]
         [TestCase(200_000, 6.5, 30, 1264.14)]
-        public void CalculateCorrectMonthlyRepayment_Parameters(decimal principal, decimal interestRate, int termInYears, decimal expectedMonthlyPayment)
+        [TestCase(200_000, 10, 30, 1755.14)]
+        [TestCase(500_000, 10, 30, 4387.86)]
+        public void CalculateCorrectMonthlyRepayment(decimal principal,
+                                                     decimal interestRate,
+                                                     int termInYears,
+                                                     decimal expectedMonthlyPayment)
         {
-            var _sut = new LoanRepaymentCalculator();
-            var _monthlyPayment = _sut.CalculateMonthlyRepayment(new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
-        
-            Assert.That(_monthlyPayment,Is.EqualTo(expectedMonthlyPayment));
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                                     new LoanAmount("USD", principal), 
+                                     interestRate, 
+                                     new LoanTerm(termInYears));
+
+            Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
         }
 
-        [Test]
-        [TestCase(200_000, 6.5, 30,ExpectedResult= 1264.14)]
-        public decimal CalculateCorrectMonthlyRepayment_Expected(decimal principal, decimal interestRate,
-            int termInYears)
-        {
-            var _sut = new LoanRepaymentCalculator();
-            return _sut.CalculateMonthlyRepayment(new LoanAmount("USD", principal), interestRate,
-                new LoanTerm(termInYears));
 
+        [Test]
+        public void CalculateCorrectMonthlyRepayment_Combinatorial(
+            [Values(100_000, 200_000, 500_000)]decimal principal,
+            [Values(6.5, 10, 20)]decimal interestRate,
+            [Values(10, 20, 30)]int termInYears)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
         }
 
-        [Test]
-        [TestCaseSource(typeof(MonthlyRepaymentTestData),"TestCases")]
-        public decimal CalculateCorrectMonthlyRepayment_Centralized(decimal principal, decimal interestRate,
-            int termInYears)
-        {
-            var _sut = new LoanRepaymentCalculator();
-            return _sut.CalculateMonthlyRepayment(new LoanAmount("USD", principal), interestRate,
-                new LoanTerm(termInYears));
 
+        [Test]
+        [Sequential]
+        public void CalculateCorrectMonthlyRepayment_Sequential(
+                    [Values(200_000, 200_000, 500_000)]decimal principal,
+                    [Values(6.5, 10, 10)]decimal interestRate,
+                    [Values(30, 30, 30)]int termInYears,
+                    [Values(1264.14, 1755.14, 4387.86)]decimal expectedMonthlyPayment)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+
+            Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
+        }
+
+
+        [Test]
+        public void CalculateCorrectMonthlyRepayment_Range(
+            [Range(50_000, 1_000_000, 50_000)]decimal principal,
+            [Range(0.5, 20.00, 0.5)]decimal interestRate,
+            [Values(10, 20, 30)]int termInYears)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
         }
     }
 }
